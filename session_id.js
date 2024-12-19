b435c56af488f71f826c768e00cd4837dc874e8f
Nimsara3838383 <112757253+Nimsara3838383@users.noreply.github.com>
2024-12-19 19:28:06 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&GRMQ0ajL#o-NsTzQDLoY2_5y4QSR55PZRSHIHqSIHo91EG7KjzBg",
 };
